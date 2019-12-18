13bed1e5412f5efe693ddb867c24d211476cf961
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-19 09:44:42 +1000

    What is a Cors Policy

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const url1 = "https://ipapi.co/json/"
 var ipAddress = ""
+var fraudScore = ""
 
 
 
 requestIP()
-hacky()
 
 
 function requestIP() {
@@ -25,19 +25,17 @@ function requestIP() {
         let latitude = data.latitude
         let longitude = data.longitude
         console.log("IP Address:  " + ipAddress)
+
         var myJSON = JSON.stringify(data)
-        document.getElementById("ipArea").innerHTML = `Your Fake IP: ${ipAddress}`
+        document.getElementById("ipArea").innerHTML = `Your IP: ${ipAddress}`
         document.getElementById("textArea").innerHTML = `${myJSON}`
 
         let mapSRC = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAr3-W5QaQSP93-XOj7c1eWVcWCM_UErlU&q=" + `${latitude}` + "%2C" + `${longitude}` + "&zoom=12"
         document.getElementById("map").src = mapSRC
         console.log("Google Maps link:  " + mapSRC)
 
-        let ipCheckSRC = "https://www.ipqualityscore.com/free-ip-lookup-proxy-vpn-test/lookup/" + `${ipAddress}`
-        document.getElementById("myIframe").src = ipCheckSRC
-        console.log(ipCheckSRC)
-        
-        requestIP2()
+        iframeProxyCheck()
+
       })
     .catch(function(error) {
       console.log(error)
@@ -46,49 +44,32 @@ function requestIP() {
 
 // Proxy Check
 
-var apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM5OCwxNTc2Njk1Njg5LDIwMDBd.hsk_EHDv8a3nNDe4XAk8BnqLjoapgLBHPbKpgRUB7VI"
-// var urlBuild = "https://www.iphunter.info:8082/v1/ip/" + `${ipAddress}`
-// var url2 = urlBuild
-var url2 = "https://ip.teoh.io/vpn-detection"
-
-function requestIP2() {
-    function createNode(element) {
-        return document.createElement(element)
-    }
-    
-    function append(parent, el) {
-      return parent.appendChild(el)
-    }
-    
-    const ul = document.getElementById('query')
-    fetch(url2, { 'X-Key': `${apiKey}`},  {mode: 'no-cors'})
-    .then((resp) => resp.json())
-    .then(function(data) {
-        // var merged = [].concat.apply([], data)
-        console.log("Proxy Status:  " + data)
-        // let usingProxy = data.proxy
-        // console.log(usingProxy)
-        var myJSON = JSON.stringify(data)
-        // document.getElementById("ipArea").innerHTML = `${usingProxy}`
-        document.getElementById("textArea").innerHTML = `${myJSON}`
-      })
-    .catch(function(error) {
-      console.log(error)
-    }) 
-}
 
 function frameload(){
-    
-        alert("iframe loaded")
+    var iframe = document.getElementById("myIframe")
+
+    alert("iframe loaded")
+    console.log("iFrame loaded Successfully")
        
+    // var fraudScore = iframe.getElementsByClassName('partner-markets')
+    // document.getElementById("fraudText").innerHTML = `${fraudScore}`
+
+    var test = iframe.getElementById( 'partner-markets' )
+    console.log( test.textContent )
+
+
+    // var elmnt = iframe.contentWindow.document.getElementById('partner-markets')[0]
+    // elmnt.innerHTML.focus()
+
+    console.log(fraudScore)
 }
 
 // Iframe Test
-function hacky(){
+function iframeProxyCheck(){
     var iframe = document.getElementById("myIframe")
 
-    //For ex; say you have a button called mybutton in thispage.html page
-    //you can access it as follows.
-    // var sendButton = document.getElementsByClassName('icon-arrow-right2')
-    // sendButton.click()
+    let ipCheckSRC = "https://www.ipqualityscore.com/free-ip-lookup-proxy-vpn-test/lookup/" + `${ipAddress}`
+    iframe.src = ipCheckSRC
+    console.log("iFrame src link:  " + ipCheckSRC)
 }
+
