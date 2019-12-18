febb25b47e9be4eb8f0496dcd31eab1587bf9171
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-19 06:55:24 +1000

    Iframe Hacks

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
-const url1 = "https://api.codetabs.com/v1/proxy?quest=https://proxycheck.io/v2"
-const url2 = "https://ipapi.co/json/"
+const url1 = "https://ipapi.co/json/"
+var ipAddress = ""
+
+
 
 requestIP()
+hacky()
+
 
 function requestIP() {
     function createNode(element) {
@@ -17,21 +21,37 @@ function requestIP() {
     .then((resp) => resp.json())
     .then(function(data) {
         console.log(data)
-        // let usingProxy = data.proxy
-        // console.log(usingProxy)
+        ipAddress = data.ip
+        let latitude = data.latitude
+        let longitude = data.longitude
+        console.log("IP Address:  " + ipAddress)
         var myJSON = JSON.stringify(data)
-        // document.getElementById("ipArea").innerHTML = `${usingProxy}`
+        document.getElementById("ipArea").innerHTML = `Your Fake IP: ${ipAddress}`
         document.getElementById("textArea").innerHTML = `${myJSON}`
+
+        let mapSRC = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAr3-W5QaQSP93-XOj7c1eWVcWCM_UErlU&q=" + `${latitude}` + "%2C" + `${longitude}` + "&zoom=12"
+        document.getElementById("map").src = mapSRC
+        console.log("Google Maps link:  " + mapSRC)
+
+        let ipCheckSRC = "https://www.ipqualityscore.com/free-ip-lookup-proxy-vpn-test/lookup/" + `${ipAddress}`
+        document.getElementById("myIframe").src = ipCheckSRC
+        console.log(ipCheckSRC)
+        
+        requestIP2()
       })
     .catch(function(error) {
       console.log(error)
-      requestIP2()
     }) 
 }
 
-// Fallback Request
+// Proxy Check
 
-function requestIP2(){
+var apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzM5OCwxNTc2Njk1Njg5LDIwMDBd.hsk_EHDv8a3nNDe4XAk8BnqLjoapgLBHPbKpgRUB7VI"
+// var urlBuild = "https://www.iphunter.info:8082/v1/ip/" + `${ipAddress}`
+// var url2 = urlBuild
+var url2 = "https://ip.teoh.io/vpn-detection"
+
+function requestIP2() {
     function createNode(element) {
         return document.createElement(element)
     }
@@ -41,24 +61,34 @@ function requestIP2(){
     }
     
     const ul = document.getElementById('query')
-    fetch(url2)
+    fetch(url2, { 'X-Key': `${apiKey}`},  {mode: 'no-cors'})
     .then((resp) => resp.json())
     .then(function(data) {
-        console.log(data)
-        let ipAddress = data.ip
-        let latitude = data.latitude
-        let longitude = data.longitude
-        console.log("Secondary Request  " + ipAddress)
+        // var merged = [].concat.apply([], data)
+        console.log("Proxy Status:  " + data)
+        // let usingProxy = data.proxy
+        // console.log(usingProxy)
         var myJSON = JSON.stringify(data)
-        document.getElementById("ipArea").innerHTML = `Your Fake IP: ${ipAddress}`
+        // document.getElementById("ipArea").innerHTML = `${usingProxy}`
         document.getElementById("textArea").innerHTML = `${myJSON}`
-        document.getElementById("map").src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAr3-W5QaQSP93-XOj7c1eWVcWCM_UErlU&q=" + `${latitude}` + "%2C" + `${longitude}` + "&zoom=12"
       })
     .catch(function(error) {
       console.log(error)
-      document.getElementById("ipArea").innerHTML = "IDK Whats going on"
     }) 
 }
 
+function frameload(){
+    
+        alert("iframe loaded")
+       
+}
 
-// https://www.google.com/maps/embed/v1/place?key=AIzaSyAr3-W5QaQSP93-XOj7c1eWVcWCM_UErlU&q=LONGITUDE%2CLATITUDE&zoom=12
+// Iframe Test
+function hacky(){
+    var iframe = document.getElementById("myIframe")
+
+    //For ex; say you have a button called mybutton in thispage.html page
+    //you can access it as follows.
+    // var sendButton = document.getElementsByClassName('icon-arrow-right2')
+    // sendButton.click()
+}
